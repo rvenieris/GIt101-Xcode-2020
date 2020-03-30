93d24de2e5d6ccd3012b2abc834aa3ddc4452fa6
Ricardo Venieris <rvenieris@gmail.com>
2020-03-30 15:38:29 -0300

    Inicialização do Projeto

---
+++ MeuGitPrj101/MeuGitPrj101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeuGitPrj101
+//
+//  Created by Ricardo Venieris on 30/03/20.
+//  Copyright © 2020 Ricardo Venieris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
