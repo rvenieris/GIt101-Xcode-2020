ed2476bf21e41a6a78db957c0c5d252ab6ae38bb
Mohamed <lucasctravassos@gmail.com>
2020-03-30 16:46:18 -0300

    deletei

--- MeuGitPrj101/MeuGitPrj101/ViewController.swift
+++ MeuGitPrj101/MeuGitPrj101/ViewController.swift
@@ -15,40 +15,9 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 		
 		
-		print("O projeto já começou")
-        print("Theo passou pro aqui")
-		print("Victor Duarte passou por aqui?")
-		print("Ricardo Passou por aqui")
-        print ("Lucas Araujo passou por aqui!")
-        print("Mohamed realmente passou por aqui e espirrou")
-		
-        print("Lucas Frazão passou por aqui")
-		
-		print("Ricardo Passou por aqui parte 2")
-
-        
-        print ("kassio esteve aqui")
-        
-        print("Olááá")
-        print("Lucas Lima esteve aqui")
-        
-        print("Diogo mandou saudações tricolores")
-        print("Olááá")
-        print("Yuri passou aqui")
-        print("geetai passou por aqui")
-	}
-	
-	
-	
-	
-	func funcaoDoRicardo() {
-		
-	}
-	
-	
-	func funcaodoKassio() {
-		
-	}
+		print("coronavairus")
+    }
+    
 
 
 }
